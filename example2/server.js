8ad759f8508025b9f68c7b0de162e651b2de4738
Alexander <tim96@yandex.ru>
2016-02-14 10:45:35 +0400

    Move 'port' value to variable.

--- example2/server.js
+++ example2/server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var url = require("url");
+var port = 8888;
 
 function start(route, handle) {
   function onRequest(request, response) {
@@ -9,8 +10,8 @@ function start(route, handle) {
     route(handle, pathname, response);
   }
 
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+  http.createServer(onRequest).listen(port);
+  console.log("Server has started on " + port + ' port');
 }
 
 exports.start = start;
