2b64401f011c50ed90224e8567af32adb43e47e9
Alexander <tim96@yandex.ru>
2016-02-14 10:57:51 +0400

    Example how to determine operation system.

--- example2/requestHandlers.js
+++ example2/requestHandlers.js
@@ -1,15 +1,22 @@
 var exec = require("child_process").exec;
+var os = require('os');
 
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  exec("find /",
-    { timeout: 10000, maxBuffer: 20000*1024 },
-    function (error, stdout, stderr) {
-      response.writeHead(200, {"Content-Type": "text/plain"});
-      response.write(stdout);
-      response.end();
-    });
+    if (isLinux()) {
+        exec("find /",
+            {timeout: 10000, maxBuffer: 20000 * 1024},
+            function (error, stdout, stderr) {
+                response.writeHead(200, {"Content-Type": "text/plain"});
+                response.write(stdout);
+                response.end();
+            });
+    } else {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello Start");
+        response.end();
+    }
 }
 
 function upload(response) {
@@ -17,6 +24,19 @@ function upload(response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello Upload");
   response.end();
+}
+
+function isWin()
+{
+    // or using 'os' module
+    var isWin = /^win/.test(process.platform);
+    return isWin;
+}
+
+function isLinux()
+{
+    var type = os.platform();
+    return type == 'linux';
 }
 
 exports.start = start;
