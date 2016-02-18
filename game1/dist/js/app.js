b1123950ebb124c3ed58d7ba96e7a5359b85ad7e
Alexander <tim96@yandex.ru>
2016-02-18 09:51:23 +0400

    Start code new app. Change main layout, add 'init' js function.

--- game1/dist/js/app.js
+++ game1/dist/js/app.js
@@ -1,8 +1,29 @@
 jQuery(function($) {
     'use strict';
 
-    var App = function() {
-        
-    }
+    var App = {
 
+        gameId: 0,
+        myRole: '',
+        mySocketId: '',
+        currentRound: 0,
+
+        init : function() {
+            console.log('init call');
+
+            App.cacheElements();
+        },
+
+        cacheElements : function() {
+            console.log('cacheElements call');
+
+            App.$doc = $(document);
+
+            // Templates
+            App.$gameArea = $('#gameArea');
+            App.$templateIntroScreen = $('#intro-screen-template').html();
+        }
+    };
+
+    App.init();
 });
