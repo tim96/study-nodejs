29025ebd366d829169fb1be0a9285bec478f9f10
Alexander <tim96@yandex.ru>
2016-02-20 15:03:19 +0400

    Add socket.io 'on connection' example.

--- chat/index.js
+++ chat/index.js
@@ -2,11 +2,20 @@
 // Set port value
 var port = 8888;
 
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket){
+    console.log('a user connected', socket);
 });
 
 http.listen(port, function(){
