3c6117cbe56ebc6c3be9c2ec69ea944414a0ff45
Alexander <tim96@yandex.ru>
2016-02-22 11:18:35 +0400

    Add new event to wait new messages.

--- chat/index.js
+++ chat/index.js
@@ -15,9 +15,20 @@ app.get('/', function(req, res){
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
-    console.log('a user connected', socket);
+    // console.log('a user connected', socket);
+
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
 
-http.listen(port, function(){
-    console.log('listening on *:' + port);
+http.listen(process.env.PORT || port, function(){
+    console.log('listening on *:' + (process.env.PORT || port));
 });
