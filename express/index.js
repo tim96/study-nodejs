be2fbdb8f5d9f4cecf94d0bd5cf7c6ab791ab538
Alexander <tim96@yandex.ru>
2016-02-23 10:25:24 +0400

    Add example of express framework.

--- express/index.js
+++ express/index.js
@@ -1,3 +1,15 @@
 
 // Set port value
 var port = 8888;
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
+
